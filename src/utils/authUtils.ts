128f8ed6fccc3e9cbfddcf023987ff18b5951121
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-15 17:51:53 +0200

    Adding files: constants.ts and authUtils.ts. Added access information in both files.

---
+++ src/utils/authUtils.ts
@@ -0,0 +1,13 @@
+const TOKEN_KEY = 'STORAGE_KEY_ACCESS_TOKEN';
+
+export function getAccessToken(): string | null {
+  return localStorage.getItem(TOKEN_KEY);
+}
+
+export function setAccessToken(token: string): void {
+  localStorage.setItem(TOKEN_KEY, token);
+}
+
+export function clearAccessToken(): void {
+  localStorage.removeItem(TOKEN_KEY);
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,7 @@
 /**
  * Holds the API URL
  */
+
+export const BASE_URL = 'https://v2.api.noroff.dev';
+export const NOROFF_API_KEY_ENDPOINT = '/auth/create-api-key';
+export const STORAGE_KEY_API_KEY = 'noroff_api_key';
