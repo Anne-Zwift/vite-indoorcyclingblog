c9ee1fb320faa49d7fad948dcf5a3478111c9f77
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-16 11:47:41 +0200

    1B: Implement Application Store.  Adding login, logout and setRouter functions.

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,5 +1,40 @@
-/**
- * Contains functions and logic for managing application state, including authentication status login, logout and user data.
- * Each key is a URL path and the value is a function.
- * @returns corresponding page element.
- */
+import type { Profile } from "../types/Profile";
+import { getAccessToken, clearAccessToken, setAccessToken } from "./authUtils";
+//router import
+
+const hasToken = !!getAccessToken();
+
+export const state = { 
+  isLoggedIn: hasToken, 
+  userProfile: null as Profile | null, 
+  router: null as any,//for the router
+}
+
+export function login(token: string, profile: Profile): void {
+  if (!token || !profile || !profile.email) {
+    console.error("Login failed: Invalid token or profile data received.");
+    return;
+  }
+
+  setAccessToken(token);
+  state.isLoggedIn = true;
+  state.userProfile = profile;
+
+  if (state.router) {
+    state.router.navigate('/profile')
+  }
+}
+
+export function logout(): void {
+  clearAccessToken();
+  state.isLoggedIn = false;
+  state.userProfile = null;
+
+  if (state.router) {
+    state.router.navigate('/login')
+  }
+}
+
+export function setRouter(routerInstance: any): void {
+  state.router = routerInstance;
+}
