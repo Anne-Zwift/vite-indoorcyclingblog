4e2219868eceb2fc3b03b79b72b995ffaa22ef11
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-21 15:53:14 +0200

    Add STORAGE_KEY_PROFILE constant for user profile storage.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,4 @@
 export const BASE_URL = 'https://v2.api.noroff.dev';
 export const NOROFF_API_KEY_ENDPOINT = '/auth/create-api-key';
 export const STORAGE_KEY_API_KEY = 'noroff_api_key';
+export const STORAGE_KEY_PROFILE = 'app_user_profile';
