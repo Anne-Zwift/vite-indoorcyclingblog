b4816b709009cc88c0704ca8ca84d699282c67f7
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-24 13:47:39 +0200

    Fix to the path, removing a / before auth.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,7 @@
  */
 
 export const BASE_URL = 'https://v2.api.noroff.dev';
-export const NOROFF_API_KEY_ENDPOINT = '/auth/create-api-key';
+export const NOROFF_API_KEY_ENDPOINT = 'auth/create-api-key';
 export const STORAGE_KEY_API_KEY = 'noroff_api_key';
 export const STORAGE_KEY_PROFILE = 'app_user_profile';
+export const STORAGE_KEY_ACCESS_TOKEN = 'noroff_access_token';
