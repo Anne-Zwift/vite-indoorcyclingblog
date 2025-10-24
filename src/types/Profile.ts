352ed673747f21e3f6d476d72d851acad40c11a5
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-24 14:10:00 +0200

    Refactor Profile type: update bio to allow null values and ensure accessToken is included in the Profile interface.

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -3,12 +3,15 @@ import type { Media } from "./Media";
 export interface Profile {
   name: string;
   email: string;
-  bio: string;
+  bio: string | null;
   avatar: Media;
   banner: Media;
+  accessToken: string;
   _count: {
     posts: number;
     followers: number;
     following: number;
   };
 }
+
+export type UserProfileData = Omit<Profile, 'accessToken'>;
