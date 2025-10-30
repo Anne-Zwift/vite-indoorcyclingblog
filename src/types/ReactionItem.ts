10edab604207bd060d2f7bb4511f12d3cd303dd7
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-30 11:16:10 +0100

    Refactor ReactionItem interface and add PostReaction and ReactionSummary types.

--- src/types/ReactionItem.ts
+++ src/types/ReactionItem.ts
@@ -1,5 +1,20 @@
-export interface ReactionItem {
+import type { Profile } from "./Profile";
+
+/*export interface ReactionItem {
   symbol: string;
   count: number;
   reactors: string[];
+}*/
+
+export interface PostReaction {
+  id: number;
+  symbol: string;
+  user: {
+    name: string;
+  } | null;
+}
+export interface ReactionSummary {
+  symbol: string;
+  count: number;
+  reactors: Profile[];
 }
