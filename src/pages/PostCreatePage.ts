770ce02ab593013a8ab28ca9a10776488b5d51a0
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-11-07 11:40:52 +0100

    Update to the export function PostCreatePage.

--- src/pages/PostCreatePage.ts
+++ src/pages/PostCreatePage.ts
@@ -1,9 +1,13 @@
-import { createPost } from "../api/Client";
 import { PostForm } from "../components/PostForm";
+import { handlePostCreation } from "../utils/postUtils";
+
 
 /**
  * Renders the PostForm component for creating a new post.
  */
 export function PostCreatePage(_param?: string): HTMLDivElement {
-  return PostForm();
+  return PostForm({
+    onSubmit: handlePostCreation,
+    submitText: 'Create Post'
+  });
 }
