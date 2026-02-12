a7fafe37a1d7c799a8ea1dca4c515daa96c7c0c3
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2026-02-12 16:32:04 +0100

    feat(tailwind) partially migrate CommentComponent.ts to Tailwind

--- src/components/CommentComponent.ts
+++ src/components/CommentComponent.ts
@@ -10,14 +10,17 @@ import { formatRelativeDate } from "../utils/dateUtils";
 export function CommentComponent(comment: CommentItem): HTMLDivElement {
   const commentContainer = document.createElement('div');
   commentContainer.classList.add('comment-item');
+  commentContainer.className = 'flex flex-col p-4 bg-(--color-primary) mt-2 border-b border-slate-100 last:border-0 w-full max-w-2xl mx-auto mb-4'
 
   const header = document.createElement('div');
   header.classList.add('comment-header');
+  header.className = 'flex justify-between items-center text-xs text-slate-500';
 
   const authorLink = document.createElement('a');
   authorLink.href = `/#/profile/${comment.author.name}`;
   authorLink.textContent = `@${comment.author.name}`;
   authorLink.classList.add('comment-author-link');
+  authorLink.className = 'font-bold text-(--color-secondary) hover:underline transition-opacity hover:opacity-80';
 
   const dateSpan = document.createElement('span');
   dateSpan.textContent = formatRelativeDate(comment.created);
@@ -28,6 +31,7 @@ export function CommentComponent(comment: CommentItem): HTMLDivElement {
   const bodyText = document.createElement('p');
   bodyText.textContent = comment.body;
   bodyText.classList.add('comment-body');
+  bodyText.className = 'text-sm lg:text-base text-(--color-p-text) text-left mt-2 break-words';
 
   commentContainer.append(header, bodyText);
 
